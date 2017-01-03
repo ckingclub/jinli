b02deb096dc81c546744e6199e7f5e4ab9cf41f6
wujiangtao <379605072@qq.com>
2017-01-03 21:59:32 +0800

    thirdparty and login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,35 @@
+
+// 引入 QCloud 小程序增强 SDK
+var qcloud = require('../../sdkthirdparty/index');
+// 引入配置
+var config = require('../../config');
+
+// 显示繁忙提示
+var showBusy = text => wx.showToast({
+    title: text,
+    icon: 'loading',
+    duration: 10000
+});
+
+// 显示成功提示
+var showSuccess = text => wx.showToast({
+    title: text,
+    icon: 'success'
+});
+
+// 显示失败提示
+var showModel = (title, content) => {
+    wx.hideToast();
+
+    wx.showModal({
+        title,
+        content: JSON.stringify(content),
+        showCancel: false
+    });
+};
+
 var app = getApp()
+
 Page({
   data:{
     confirm:'授权登陆',
@@ -7,65 +38,45 @@ Page({
     disabled:false,
     userInfo:{}
   },
-    //事件处理函数
-  comfim: function() {
-    wx.navigateTo({
-      url: '../logs/logs',
-    })
-  },
+
+  doLogin() {
+        showBusy('正在登录');
+
+        // 登录之前需要调用 qcloud.setLoginUrl() 设置登录地址，不过我们在 app.js 的入口里面已经调用过了，后面就不用再调用了
+        qcloud.login({
+            success(result) {
+                showSuccess('登录成功');
+                wx.navigateTo({
+                  url: '../logs/logs',
+                });
+                console.log('登录成功', result);
+            },
+
+            fail(error) {
+                showModel('登录失败', error);
+                console.log('登录失败', error);
+            }
+        });
+    },
+    
   cancel:function() {
      wx.navigateTo({
       url: '../logs/logs', 
     }) 
   },
-  login:function(){
-    var that = this
-    wx.login({
-      success: function(res){
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: 'https://api.jinlibuyi.com',
-            data: {
-              code: res.code
-            },
-            method: 'GET',
-            success: function (res) {  
-            try {  
-             wx.setStorageSync('id_token', res.data.id_token)  
-             } catch (e) {  
-            }  
-            wx.navigateTo({  
-            url: '../logs/logs'  
-            })  
-            console.log(res.data);  
-            },  
-            fail: function (res) {  
-                console.log(res.data);  
-                console.log('is failed')  
-            } 
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      },
-      fail: function() {
-        console.log('获取用户登录态失败！' + res.errMsg)
-      },
-      complete: function() {
-        console.log('获取用户登录成功！' + res.errMsg)
-      }
-    })
-  },
   onLoad: function () {
-    console.log('onLoad')
     var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.setData({
+                userInfo:res.userInfo
+              })
+            }
+          })
+        }
       })
-    })
   }
 })
